572e109db7b09c7d32fd8c15c72345c8553e58be
ARKXWOLF <2310011@bart.ca>
2026-01-13 23:46:01 -0500

    still working on cam

--- Assets/Scripts/FreeCamMovement.cs
+++ Assets/Scripts/FreeCamMovement.cs
@@ -1,30 +1,73 @@
+using System;
 using UnityEngine;
+using UnityEngine.Animations;
 using UnityEngine.InputSystem;
 
 public class FreeCamMovement : MonoBehaviour
 {
+    private InputActionMap action;
 
+    [SerializeField] private float zoomSpeed = 10f;
+    [SerializeField] private float RotationSpeed = 10f;
+    [SerializeField] private float CamMoveSpeed = 10f;
+
+
+    private void OnEnable()
+    {
+        action.Enable();
+    }
+    private void OnDisable()
+    {
+        action.Disable();
+    }
     void Start()
     {
+        InputActionMap playerInput = GetComponent<PlayerInput>().currentActionMap;
+        action = playerInput;
 
     }
 
     void Update()
     {
-
-    }
-    private void Zoom()
+         switch (action.actions.ToString())
+         {
+            case "Zoom":
+                Zoom(action.);
+                break;
+            case "Move Around":
+                MoveCamera(action.ReadValue<Vector2>());
+                break;
+            case "Rotate":
+                RotateCamera(action.ReadValue<KeyCode>());
+                break;
+            }
+        }
+    private void Zoom(int value)
     {
-
+        Camera cam = GetComponent<Camera>();
+        cam.fieldOfView = value * zoomSpeed * Time.deltaTime;
     }
 
-    private void MoveCamera()
+    private void MoveCamera(Vector2 value)
     {
-
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.MovePosition(new Vector3(value.x, value.y, 0) * CamMoveSpeed * Time.deltaTime);
     }
 
-    private void RotateCamera()
+    private void RotateCamera(KeyCode value)
     {
-
+        switch (value)
+        {
+            case KeyCode.Q:
+                transform.Rotate(Vector3.up, -RotationSpeed * Time.deltaTime);
+                break;
+            case KeyCode.E:
+                transform.Rotate(Vector3.up, RotationSpeed * Time.deltaTime);
+                break;
+            case KeyCode.Mouse2:
+                Vector2 mousepos = Mouse.current.position.ReadValue();
+                transform.Rotate(Vector3.up, mousepos.y * Time.deltaTime);
+                break;
+        }
     }   
 }
