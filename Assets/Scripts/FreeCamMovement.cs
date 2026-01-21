c37ad04e62886ea3bdfc67fd01c9fcf4d96d8854
Etienne <2310004@bart.ca>
2026-01-20 22:39:43 -0500

    fixed broken shit

--- Assets/Scripts/FreeCamMovement.cs
+++ Assets/Scripts/FreeCamMovement.cs
@@ -10,27 +10,6 @@ public class FreeCamMovement : MonoBehaviour
 {
     [SerializeField]private CamPivot ParentPos;
 
-<<<<<<< HEAD
-=======
-    private float yaw;
-    private float pitch;
-
-    [SerializeField] private float zoomSpeed = 10f;
-    [SerializeField] private float RotationSpeed = 10f;
-    [SerializeField] private float CamMoveSpeed = 10f;
-
-
-    private void OnEnable()
-    {
-        action = GetComponent<PlayerInput>().currentActionMap;
-
-        action.Enable();
-    }
-    private void OnDisable()
-    {
-        action.Disable();
-    }
->>>>>>> 3cc72d56af1f30c139140aca9173c1f53f8ba39f
     void Start()
     {
         if (Physics.Raycast(transform.position, transform.forward, out RaycastHit result))
@@ -43,57 +22,6 @@ public class FreeCamMovement : MonoBehaviour
     public void Zoom(Vector2 _v)
     {
         transform.Translate(Vector3.forward * (_v.y - _v.x));
-    }
-
-    void Update()
-    {
-        Vector3 point = Vector3.zero;
-<<<<<<< HEAD
-=======
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit result))
-        {
-            point = result.point;
-        }
-
-        yaw += RotationInput.x * RotationSpeed * Time.deltaTime;
-        pitch -= RotationInput.y * RotationSpeed * Time.deltaTime;
-        pitch = Mathf.Clamp(pitch, -80f, 80f);
-
-        UpdateCamera(point, Vector3.Distance(transform.position, point));
-
-        transform.position = new Vector3(transform.position.x + XMovement.x * CamMoveSpeed * Time.deltaTime, transform.position.y, transform.position.z + XMovement.y * CamMoveSpeed * Time.deltaTime);
-
-    }
-
-    private void UpdateCamera(Vector3 point, float distance)
-    {
-        Quaternion rotation = Quaternion.Euler(pitch, 0f, yaw);
-        Vector3 offset = rotation * Vector3.back * distance;
-
-        transform.position = point + offset;
-        transform.rotation = rotation;
-    }
-
-    private void OnZoom(InputValue value)
-    {
-        Camera cam = GetComponent<Camera>();
-        var v = value.Get<float>();
-        cam.fieldOfView = v * zoomSpeed * Time.deltaTime;
-    }
-
-    private void OnCamMove(InputValue value)
-    {
-
-        XMovement = value.Get<Vector2>();
-    }
-
-    private void OnRotate(InputValue value)
-    {
-        var v = value.Get<float>();
-
-        print(value);
-        RotationInput = value.Get<Vector2>();
->>>>>>> 3cc72d56af1f30c139140aca9173c1f53f8ba39f
     }
 
 }
